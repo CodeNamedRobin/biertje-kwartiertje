0813c99175379cc87add575f4be83c596dfb2f96
CodeNamedRobin <robinvandijke2002@gmail.com>
2024-02-19 11:13:23 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+body, html {
+    height: 100%;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: Arial, sans-serif;
+}
+
+#countdown {
+    font-size: 3em;
+}
+
+#message {
+    display: none;
+    font-size: 2em;
+}
